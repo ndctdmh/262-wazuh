c4d1a36ace8121d7ae03f470778821df2a617ea2
ndctdmh <32846513+ndctdmh@users.noreply.github.com>
2020-03-30 22:15:59 -0400

    Update internal_options1.sh

--- internal_options1.sh
+++ internal_options1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "Running the Cyber 262 setup1.sh"
 echo "syscheck.debug=2" > /var/ossec/etc/local_internal_options.conf
 echo "agent.debug=2" >> /var/ossec/etc/local_internal_options.conf
 echo "rootcheck.sleep=0" >> /var/ossec/etc/local_internal_options.conf
