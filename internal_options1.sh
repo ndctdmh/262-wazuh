0715fbf1970244816e66431778d4c8e906ad5c0a
ndctdmh <32846513+ndctdmh@users.noreply.github.com>
2020-03-30 22:06:55 -0400

    Update internal_options1.sh

--- internal_options1.sh
+++ internal_options1.sh
@@ -2,5 +2,5 @@
 
 echo "syscheck.debug=2" > /var/ossec/etc/local_internal_options.conf
 echo "agent.debug=2" >> /var/ossec/etc/local_internal_options.conf
-echo "rootcheck.sleep=0" >> var/ossec/etc/local_internal_options.conf
+echo "rootcheck.sleep=0" >> /var/ossec/etc/local_internal_options.conf
 echo "syscheck.sleep=0" >> /var/ossec/etc/local_internal_options.conf
